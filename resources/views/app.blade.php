d205edbe908a4af1ad17d5661e32b89ee0f8f4fe
Gregori Piñeres <gregori.pineres02@gmail.com>
2023-09-30 17:18:51 -0400

    remmove HTML comments

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,16 +7,12 @@
         <meta name="apple-mobile-web-app-status-bar" content="#161519" />
         <meta name="description" content="Enhance your English speaking skills by using short, practical sentences. Ideal for students and professionals looking to strengthen their command of English" />
         <title inertia>Eloquentclub.com</title>
-
-        <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
         <link rel="manifest" href="/site.webmanifest">
-
-        <!-- Scripts -->
         @routes
         @viteReactRefresh
         @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
